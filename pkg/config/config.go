d97f718d48a712fb3069c6136174875c17b0c8fa
Lei Gong <lgong@alauda.io>
2019-07-18 13:43:31 +0800

    Add prototype

---
+++ pkg/config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/rest"
+	"k8s.io/klog"
+	"net/url"
+)
+
+const (
+	DefaultAcceptContentType = ""
+	DefaultContentType       = ""
+	DefaultUserAgent         = "kube-rest"
+	DefaultApiPath           = ""
+	DefaultClientBurst       = 30.0
+	DefaultClientQPS         = 20.0
+)
+
+var (
+	Scheme = runtime.NewScheme()
+	Codecs = serializer.NewCodecFactory(Scheme)
+)
+
+func GetDefaultConfig(server string) (*rest.Config, error) {
+	_, err := url.Parse(server)
+	if nil != err {
+		return nil, err
+	}
+	config := &rest.Config{
+		Host: server,
+		ContentConfig: rest.ContentConfig{
+			AcceptContentTypes:   DefaultAcceptContentType,
+			ContentType:          DefaultContentType,
+			NegotiatedSerializer: Codecs,
+			GroupVersion:         &schema.GroupVersion{},
+		},
+		UserAgent: DefaultUserAgent,
+		APIPath:   DefaultApiPath,
+		Burst:     DefaultClientBurst,
+		QPS:       DefaultClientQPS,
+	}
+	return config, nil
+}
+
+func GetConfigOrDie(server string) *rest.Config {
+	cfg, err := GetDefaultConfig(server)
+	if nil != err {
+		klog.Fatalf("unable to get kubeconfig, err=%s", err.Error())
+		return nil
+	}
+	return cfg
+}
---
+++ pkg/rest/interface.go
@@ -0,0 +1,60 @@
+package rest
+
+import (
+	"alauda/kube-rest/pkg/types"
+	"context"
+
+	types2 "k8s.io/apimachinery/pkg/types"
+)
+
+// Object is the object entity for a rest request.
+// It knows how to get request url, parsing object and do the deepcopy.
+type Object interface {
+	AbsPath() string
+	AbsObjPath() string
+	Data() ([]byte, error)
+	Parse(bt []byte) error
+}
+
+// Patch is a patch that can be applied to a rest object.
+type Patch interface {
+	// Type is the PatchType of the patch.
+	Type() types2.PatchType
+	// Data is the raw data representing the patch.
+	Data(obj Object) ([]byte, error)
+}
+
+// Reader knows how to read and list objects.
+type Reader interface {
+	// Get retrieves an obj for the given object key from the rest object.
+	// obj must be a struct pointer so that obj can be updated with the response
+	// returned by the Server.
+	Get(ctx context.Context, obj Object) error
+
+	// List retrieves list of objects for a given namespace and list options. On a
+	// successful call, Items field in the list will be populated with the
+	// result returned from the server.
+	List(ctx context.Context, obj Object, opts *types.Options) error
+}
+
+// Writer knows how to create, delete, and update rest objects.
+type Writer interface {
+	// Create saves the object obj in the rest object.
+	Create(ctx context.Context, obj Object) error
+
+	// Delete deletes the given obj from rest object.
+	Delete(ctx context.Context, obj Object, opts *types.Options) error
+
+	// Update updates the given obj in the rest object. obj must be a
+	// struct pointer so that obj can be updated with the content returned by the Server.
+	Update(ctx context.Context, obj Object) error
+
+	// Update updates the given obj in the rest object. obj must be a
+	// struct pointer so that obj can be updated with the content returned by the Server.
+	Patch(ctx context.Context, patch Patch, obj Object) error
+}
+
+type Client interface {
+	Reader
+	Writer
+}
